97cc7ce73b6e8676ffcbb73e2ff3fa0fef3a5bf4
cagatay <cgtyeo@gmail.com>
2023-01-21 23:35:27 +0300

    corsorigin

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ dbConnect();
 //Curb Cores Error by adding a header here
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*",
+    "https://mernloginbackend.onrender.com/"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
